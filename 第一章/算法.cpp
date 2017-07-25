b5194052f21a2aea1cc73d952c5c36aeff063947
fisland <whereisfisland@163.com>
2017-07-26 00:44:16 +0800

    first commit

---
+++ 第一章/算法.cpp
@@ -0,0 +1,15 @@
+#include "stdio.h"
+
+int main(int argc, char const *argv[]) {
+  // int i, sum = 0, n = 100;
+  // for (i = 1; i <=n; i++) {
+  //   /* code */
+  //   sum = sum + i;
+  // }
+  // printf("%d\n",sum);
+
+  int sum1 = 0, n1 = 100;
+  sum1 = (1 + n1) * n1/2;
+  printf("%d",sum1 );
+  return 0;
+}
